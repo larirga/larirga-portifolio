144c426a68f71b273f1c96a357001a153628d6e3
Larissa <larisssa_rga@hotmail.com>
2023-03-01 16:19:09 -0300

    testing 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>
-          TESTING 
+          TESTING 2 
         </h1>
         <a
           className="App-link"
