b76e523764333053af3e7d7270a29e202a914032
Larissa <larisssa_rga@hotmail.com>
2023-03-02 18:42:08 -0300

    fixing width home and fixing about me text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,11 +7,11 @@ export default function About() {
     <div className='about-div'>
       <p className='about-title'> About </p>
         <p>Me chamo Larissa,</p>
-        <p className='text'>sou de BH, capital de Minas Gerais. Gosto de assistir animes e doramas, de ler e jogar no meu tempo livre. 
+        <p className='text'>sou de BH, capital de Minas Gerais. Ultimamente, tenho focado meus estudos em JavaScript tanto o frontend
+        quanto o backend, adoro mexer com React.js, inclusive foi a biblioteca que usei nessa página. No meu tempo livre gosto de assistir animes e doramas, ler e jogar.
           Adoro sair, cozinhar, aprender e conversar com meus amigos e família.
-          Tenho dois gatinhos lindos, o Tony e a Shinji. Sou formada em Filosofia pela UFMG e 
-          atualmente, estudo desenvolvimento web na Trybe. Ultimamente, tenho focado meus estudos em frontend e backend, adoro
-          mexer com react, inclusive foi a biblioteca que usei nessa página.
+          Tenho dois gatinhos lindos, o Tony e a Shinji. Além disso, sou formada em Filosofia pela UFMG e 
+          atualmente, estudo desenvolvimento web na Trybe.
           Para saber mais sobre minhas hardskills acesse meu <a href="https://github.com/larirga"><b>gitHub</b></a>.
     </p>
 
