4275c49cbb0d83b50a8d2e29e3cf292429233c97
Larissa <larisssa_rga@hotmail.com>
2023-03-02 11:17:30 -0300

    Add home with image and about title

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import { Route, Switch } from "react-router-dom";
+import { Link, Route, Switch } from "react-router-dom";
 import Home from './pages/Home';
 import About from './pages/About';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className="header">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/About">About</Link>
+          </li>
+          <li>
+            <Link to="/Projects">Projects</Link>
+          </li>
+          <li>
+            <Link to="/Contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      </div>
+      <div>
       <Switch>
         <Route
           exact
@@ -17,7 +38,23 @@ function App() {
           path="/About"
           component={ About }
           />
+        <Route
+          exact
+          path="/Projects"
+          component={ Projects }
+          />
+        <Route
+          exact
+          path="/Contact"
+          component={ Contact }
+          />
       </Switch>
+      </div>
+      <div>
+        <footer>
+          <p>oi</p>
+        </footer>
+      </div>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import home_image from '../images/home_image.png'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <img className="home-image" src={ home_image } alt="home with apresentation" />
+    </div>
   )
 }
