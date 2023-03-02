74e75806e82dc0ecc3dd6bb5073b90afae50cbf2
Larissa <larisssa_rga@hotmail.com>
2023-03-02 17:47:08 -0300

    fixing about text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,18 +7,16 @@ export default function About() {
     <div className='about-div'>
       <p className='about-title'> About </p>
         <p>Me chamo Larissa,</p>
+        <p className='text'>sou de BH, capital de Minas Gerais. Gosto de assistir animes e doramas, de ler e jogar no meu tempo livre. 
+          Adoro sair, cozinhar, aprender e conversar com meus amigos e família.
+          Tenho dois gatinhos lindos, o Tony e a Shinji. Sou formada em Filosofia pela UFMG e 
+          atualmente, estudo desenvolvimento web na Trybe. Ultimamente, tenho focado meus estudos em frontend e backend, adoro
+          mexer com react, inclusive foi a biblioteca que usei nessa página.
+          Para saber mais sobre minhas hardskills acesse meu <a href="https://github.com/larirga"><b>gitHub</b></a>.
+    </p>
 
     <div className='text-about'>
-    <div className='about-picture'>
     <img className='about-picture' src={ about_image } alt="ti" />
-    </div>
-    <p>Sou de BH, capital de Minas Gerais. Gosto de assistir séries, animes e doramas, de ler e jogar no meu tempo livre. 
-    Adoro sair, cozinhar, aprender e conversar com meus amigos e família.
-    Tenho dois gatinhos lindos, o tony e a Shinji. Sou formada em Filosofia pela UFMG e 
-    atualmente, estudo desenvolvimento web na Trybe. Ultimamente, tenho focado meus estudos em frontend e backend, adoro
-    mexer com react, inclusive foi a biblioteca que usei nessa página.
-    Para saber mais sobre minhas hardSkills acesse meu <a href="https://github.com/larirga"><b>gitHub</b></a>.
-    </p>
     </div>
     </div>
   )
